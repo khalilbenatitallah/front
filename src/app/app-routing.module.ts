294d05f050014bc3a579b21b91b3adf1ab2e4b89
khalilbenatitallah <125036429+khalilbenatitallah@users.noreply.github.com>
2023-02-24 14:49:37 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShopComponent } from './shop/shop.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path:'contact',component:ContactComponent},
+  {path:'home',component:HomeComponent},
+  {path:'shop',component:ShopComponent},
+  {path:'checkout',component:CheckoutComponent},
+
+
+  {path:'cart',component:CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ShopComponent } from './shop/shop.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CartComponent } from './cart/cart.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    ShopComponent,
+    CheckoutComponent,
+    CartComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent
+
+
   ],
   imports: [
     BrowserModule,
+
     AppRoutingModule
   ],
   providers: [],
